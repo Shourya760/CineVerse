53c762c382e5fb51ba92c52f37a64ac73e24c9eb
shourya verma <shouryav760@gmail.com>
2025-10-02 17:49:40 +0530

    responsiveness

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // Make sure you have the React plugin
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  server: {
+    historyApiFallback: true, // fallback to index.html on reload
+  },
 })
